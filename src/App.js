67a45f6440e4e09511de48d27bdba2943676bea0
abdulkadiret <ethiogentelman@gmail.com>
2020-09-02 22:12:45 +0100

    Create TodoList component & import into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Components/Header';
+import TodoList from './Components/TodoList';
 
 function App() {
   return (
     <div className="todoapp">
       <Header />
+      <TodoList />
     </div>
   );
 }
---
+++ src/Components/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoList = () => {
+  return (
+    <section className="main">
+      <ul className="todo-list">
+        <li>Item 1</li>
+        <li>item 2</li>
+      </ul>
+    </section>
+  );
+};
+
+export default TodoList;

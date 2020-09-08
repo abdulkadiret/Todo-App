b92e454d0bf226412a5aa48b56b1cf72e7ad3778
abdulkadiret <ethiogentelman@gmail.com>
2020-09-08 16:33:44 +0100

    Add functionality to edit todo item value

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoItem = (props) => {
+  const [editMode, setEditMode] = useState(false);
+  const [itemNewValue, setItemNewValue] = useState(props.todo.value);
+
   const handleCompleted = () => {
     props.completeTodo(props.todo.id);
   };
@@ -9,13 +12,52 @@ const TodoItem = (props) => {
     props.deleteTodo(props.todo.id);
   };
 
+  const enableEditMode = () => {
+    setEditMode(true);
+  };
+
+  const handleItemNewValue = (e) => {
+    const editedValue = e.target.value;
+    setItemNewValue(editedValue);
+  };
+
+  const handleUpdateSubmit = (e) => {
+    if (e.keyCode === 13) {
+      setEditMode(false);
+    }
+  };
+
+  const viewStyle = {};
+  const editStyle = {};
+
+  if (editMode) {
+    viewStyle.display = 'none';
+  } else {
+    editStyle.display = 'none';
+  }
+
   return (
-    <li className={props.todo.completed ? 'completed' : ''}>
+    <li
+      className={`editing ${props.todo.completed ? 'completed' : ''}`}
+      key={props.todo.id}
+    >
       <div className="view">
         <input className="toggle" type="checkbox" onChange={handleCompleted} />
-        <label>{props.todo.value}</label>
+        <label onDoubleClick={enableEditMode} style={viewStyle}>
+          {itemNewValue}
+        </label>
         <button className="destroy" onClick={handleDelete}></button>
       </div>
+
+      <input
+        style={editStyle}
+        className="edit"
+        onKeyDown={handleUpdateSubmit}
+        onChange={handleItemNewValue}
+        type="text"
+        value={itemNewValue}
+        autoFocus
+      />
     </li>
   );
 };

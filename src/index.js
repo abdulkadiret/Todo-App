2da60ec12b2ff8b2ab4375c5d6e1cbbbc4d6cc3d
abdulkadiret <ethiogentelman@gmail.com>
2020-09-09 13:02:15 +0100

    Change index.css with todomvc-app-css style package

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'todomvc-app-css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
